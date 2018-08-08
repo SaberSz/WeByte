4b1438277210ebf6a005d6efff3839d565808d17
SaberSz <32420868+SaberSz@users.noreply.github.com>
2018-08-08 23:32:13 +0530

    cards in battles and results chnaged. Results page done.

--- CodeArena/templates/Utilities.py
+++ CodeArena/templates/Utilities.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta
 
 
 def is_day_after_current(string_input_with_date, string_input_with_time):
-    pastd = datetime.strptime(string_input_with_date, "%d/%m/%Y")
-    pastt = datetime.strptime(string_input_with_time, "%H:%M")
+    pastd = datetime.strptime(string_input_with_date, "%Y-%m-%d")
+    pastt = datetime.strptime(string_input_with_time, "%H:%M:%S")
     event_time = pastd + timedelta(hours=pastt.hour, minutes=pastt.minute)
     present = datetime.now()
     return (event_time < present)
